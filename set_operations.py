dd47fc7b7b6a03460a8d758b5f91df6d8c1dd195
abi2901 <96660328+abi2901@users.noreply.github.com>
2021-12-26 14:00:56 +0530

    created set_operations.py

---
+++ set_operations.py
@@ -0,0 +1,12 @@
+# Program to perform different set operations
+# Define three sets
+A={0,2,4,6,8}
+B={1,2,3,4,5}
+# Set union
+print("Union of A and B is",A | B)
+# Set intersection
+print("Intersection of A and B is",A & B)
+# Set difference
+print("Difference of A and B is",A - B)
+# Set symmetric difference
+print("Symmetric difference of A and B is",A ^ B)
